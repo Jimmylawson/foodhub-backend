2b1be6be0d9e5d11a8f639d332a80db663188a69
jimmy <jimoseipeace2bb@gmail.com>
2025-07-11 03:28:25 -0500

    "Done with Repo"

---
+++ src/main/java/com/food_delivery/zomato_backend/repository/DeliveryRepository.java
@@ -0,0 +1,4 @@
+package com.food_delivery.zomato_backend.repository;
+
+public interface DeliveryRepository {
+}
---
+++ src/main/java/com/food_delivery/zomato_backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.food_delivery.zomato_backend.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
