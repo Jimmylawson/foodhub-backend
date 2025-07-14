f8c33c9bb5fe0afa1b1ac5fb2530fde5350447d3
jimmy <jimoseipeace2bb@gmail.com>
2025-07-13 19:01:17 -0500

    "Created files for controllers for the endpoints"

---
+++ src/main/java/com/food_delivery/zomato_backend/controller/DeliveryController.java
@@ -0,0 +1,10 @@
+package com.food_delivery.zomato_backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/delivery")
+public class DeliveryController {
+}
---
+++ src/main/java/com/food_delivery/zomato_backend/controller/MenuItemController.java
@@ -0,0 +1,9 @@
+package com.food_delivery.zomato_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/menuItems")
+public class MenuItemController {
+}
